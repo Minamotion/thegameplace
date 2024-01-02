ca84c23958e70f65f834b443b4457e535a8b9d14
MultiLayer <multilayernowofficial@gmail.com>
2024-01-02 14:21:06 -0600

    Modified s o  m   e of the C O  D   E

--- assets/scripts/definedarkmode.js
+++ assets/scripts/definedarkmode.js
@@ -1,15 +1,18 @@
 async function DarkMode() {
+    if (localStorage.getItem('darkmode') == undefined) {
+        localStorage.setItem('darkmode', '{"active": false}')
+    }
     if (new URLSearchParams(window.location.search).has('light') && !(new URLSearchParams(window.location.search).has('dark'))) {
-        localStorage.setItem('darkmode', {"active": false});
+        localStorage.setItem('darkmode', '{"active": false}');
         console.warn("You have deactivated darkmode (BETA)");
     }
     if (new URLSearchParams(window.location.search).has('dark') || JSON.parse(localStorage.getItem('darkmode')).active) {
         document.getElementById('darkmode').innerHTML = await fetch("/assets/stylesheets/modes/dark.css").then(e => e.text());
         console.info("You're currently using dark mode")
         console.info("to deactivate darkmode use the parameter light and remove the dark parameter")
-        console.info("or you can just run \"localStorage.setItem('darkmode', {\"active\": true})\"")
+        console.info("or you can just run \"localStorage.setItem('darkmode', '{\"active\": true}')\"")
         if (!(JSON.parse(localStorage.getItem('darkmode')).active)) {
-            localStorage.setItem('darkmode', {"active": true})
+            localStorage.setItem('darkmode', '{"active": true}')
         }
     } else {
         console.warn("Use dark mode, Just add the parameter 'dark' to the url")
