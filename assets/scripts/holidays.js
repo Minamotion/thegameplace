9bce6c615148a9b8c24da75864d43889a9664710
MultiLayer <multilayernowofficial@gmail.com>
2024-01-04 16:37:19 -0600

    lkadjfsjdfihdsifidsf

--- assets/scripts/holidays.js
+++ assets/scripts/holidays.js
@@ -8,36 +8,40 @@ let missingJerry = (siteFavicon == null)
 // Thanks "https://www.w3schools.com/js/js_date_methods.asp" for the "new Date()" thing (also months & weekdays have an offset of -1)
 // The site's icon has a name, and it's Jerry, that's why every holiday's png file with Jerry in it has a suffix of "_jerry"
 
-
-if (holidayCheckTime.getMonth() === 9) {
-    // It's halloween!
-    holidayWereOn = 'halloween'
-    if(!(missingJerry)) {
-        siteFavicon.src = '/assets/images/holiday/holiday-halloween_easteregg_jerry.png'
-    } else {
-        console.error('Error at "holidays.js": ...Where is Jerry?')
+if(localStorage.getItem('fun') == 'true'){
+    if (holidayCheckTime.getMonth() == 9) {
+        // It's halloween!
+        holidayWereOn = 'halloween'
+        if(!(missingJerry)) {
+            siteFavicon.src = '/assets/images/holiday/holiday-halloween_easteregg_jerry.png'
+        } else {
+            console.error('Error at "holidays.js": ...Where is Jerry?')
+        }
+        holidayStylesheet.setAttribute('href', '/assets/stylesheets/holiday/holiday-halloween.css')
     }
-    holidayStylesheet.setAttribute('href', '/assets/stylesheets/holiday/holiday-halloween.css')
-}
-
-if (holidayCheckTime.getMonth() === 11) {
-    // It's christmas!
-    holidayWereOn = 'christmas'
-    if(!(missingJerry)) {
-        siteFavicon.src = '/assets/images/holiday/holiday-christmas_easteregg_jerry.png'
-    } else {
-        console.error('Error at "holidays.js": Where is Jerry? I thought he liked Christmas')
+    
+    if (holidayCheckTime.getMonth() == 11) {
+        // It's christmas!
+        holidayWereOn = 'christmas'
+        if(!(missingJerry)) {
+            siteFavicon.src = '/assets/images/holiday/holiday-christmas_easteregg_jerry.png'
+        } else {
+            console.error('Error at "holidays.js": Where is Jerry? I thought he liked Christmas')
+        }
+        holidayStylesheet.setAttribute('href', '/assets/stylesheets/holiday/holiday-christmas.css')
     }
-    holidayStylesheet.setAttribute('href', '/assets/stylesheets/holiday/holiday-christmas.css')
-}
-
-if (holidayCheckTime.getMonth() === 2 && holidayCheckTime.getDate() === 30) {
-    // It's my birthday!
-    holidayWereOn = 'creatorBirthday'
-    if(!(missingJerry)) {
-        siteFavicon.src = '/assets/images/holiday/holiday-birthday_easteregg_jerry.png'
-    } else {
-        console.error('Error at "holidays.js": Where is Jerry? I thought I invited him to my birthday party, where is he?')
+    
+    if (holidayCheckTime.getMonth() == 2 && holidayCheckTime.getDate() == 30) {
+        // It's my birthday!
+        holidayWereOn = 'creatorBirthday'
+        if(!(missingJerry)) {
+            siteFavicon.src = '/assets/images/holiday/holiday-birthday_easteregg_jerry.png'
+        } else {
+            console.error('Error at "holidays.js": Where is Jerry? I thought I invited him to my birthday party, where is he?')
+        }
+        holidayStylesheet.setAttribute('href', '/assets/stylesheets/holiday/holiday-creator_birthday.css')
     }
-    holidayStylesheet.setAttribute('href', '/assets/stylesheets/holiday/holiday-creator_birthday.css')
+} else {
+    holidayWereOn = "grinchmode"
+    console.error('Error at "holidays.js": Apparently the user doesn\'t like holidays (I think the user is secretly the Grinch)')
 }
