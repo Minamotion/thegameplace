3b3e98e1b01fd4c96bd82ca9083ac7a22323bf37
MultiLayer <multilayernowofficial@gmail.com>
2024-01-04 17:29:21 -0600

    ilejfkdhfkjs

---
+++ assets/scripts/jerry.js
@@ -0,0 +1,4 @@
+const jerrystuff = document.getElementById('jerrystuff')
+const jerrysaylist = ['Hello','This is my official account :D']
+
+jerrystuff.innerHTML = jerrysaylist[Math.random(jerrysaylist.length)]
