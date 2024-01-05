ebe35fe32a1d54791f1d1f47bdcf7192762a62b5
MultiLayer <multilayernowofficial@gmail.com>
2024-01-05 10:47:50 -0600

    uigyutfycrdtfvhbjkgvh

--- assets/scripts/jerry.js
+++ assets/scripts/jerry.js
@@ -7,8 +7,6 @@ let jerrysaylist=[
     '<span title="Don\'t">Did you know that pressing <span style="font-family:monospace;">ALT+F4</span> you can take a screenshot of your screen? Try it out!</span>'
 ];
 function randomInt(min,max){return Math.floor(Math.random()*(max-min+1))+min;}
-let coder = new URLSearchParams(window.location.search).get('coder')
-let s = randomInt(0,((Object.keys(jerrysaylist).length)-1))
 while (document.getElementById('jerrystuff') == null) {}
-document.getElementById('jerrystuff').innerHTML=jerrysaylist[s];
-delete coder; delete s; delete jerrysaylist;
+document.getElementById('jerrystuff').innerHTML=jerrysaylist[randomInt(0,((Object.keys(jerrysaylist).length)-1))];
+delete jerrysaylist;
