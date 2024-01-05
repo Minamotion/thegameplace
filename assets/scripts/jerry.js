07ab9c22386234ddd34e923492026eaa3d0ac9af
MultiLayer <multilayernowofficial@gmail.com>
2024-01-04 20:22:51 -0600

    edit

--- assets/scripts/jerry.js
+++ assets/scripts/jerry.js
@@ -1,6 +1,11 @@
-const jerrystuff = document.getElementById('jerrystuff')
-const jerrysaylist = ['Hello there my friend','This is my official account :D']
-function getRndInteger(min, max) {
-      return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-jerrystuff.innerHTML = jerrysaylist[getRndInteger(0, jerrysaylist.length)]
+const jerrystuff=document.getElementById('jerrystuff');
+const jerrysaylist=[
+    'Hello there!',
+    'This is my official account :D',
+    'I\'m inside your wallet',
+    'Here is a tip: Watch out for Duolingo',
+    'I need help, I think I\'m green but my color is <a href="https://www.google.com/search?q=%2379cea5">#79CEA5</a>',
+    'Did you know that pressing <span style="font-family:monospace;">ALT+F4</span> you can take a screenshot of your screen? Try it out!'
+];
+function randomInt(min,max){return Math.floor(Math.random()*(max-min+1))+min;}
+setInterval(()=>{jerrystuff.innerHTML=jerrysaylist[randomInt(0,jerrysaylist.length)]},10);
