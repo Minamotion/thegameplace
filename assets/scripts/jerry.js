9f8317572b16bfbdb1a66ea089ee358b36aec05d
MultiLayer <multilayernowofficial@gmail.com>
2024-01-04 18:11:48 -0600

    gsg

--- assets/scripts/jerry.js
+++ assets/scripts/jerry.js
@@ -1,4 +1,6 @@
 const jerrystuff = document.getElementById('jerrystuff')
-const jerrysaylist = ['Hello','This is my official account :D']
-
-jerrystuff.innerHTML = jerrysaylist[Math.random(jerrysaylist.length)]
+const jerrysaylist = ['Hello there my friend','This is my official account :D']
+function getRndInteger(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+jerrystuff.innerHTML = jerrysaylist[getRndInteger(0, jerrysaylist.length)]
