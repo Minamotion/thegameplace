9e1977eab0c8c3dfd868296ac9fb5d7466a1f95e
MultiLayer <multilayernowofficial@gmail.com>
2024-01-04 22:01:35 -0600

    LKIODJHUFG

--- assets/scripts/close.js
+++ assets/scripts/close.js
@@ -4,6 +4,6 @@ let isTheMoonAlreadyAwake = !(hours > 6 && hours < 20)
 // When page loaded if is night time then go to closed.html
 window.addEventListener('load', function(){
     if(isTheMoonAlreadyAwake){
-        this.location.assign('./closed.html')
+        this.location.assign('/closed.html')
     }
 })
--- assets/scripts/open.js
+++ assets/scripts/open.js
@@ -4,6 +4,6 @@ let isTheSunAlreadyAwake = (hours > 6 && hours < 20)
 // When page loaded if is day time then go to index.html
 window.addEventListener('load', function(){
     if(isTheSunAlreadyAwake){
-        this.location.assign('./index.html')
+        this.location.assign('/index.html')
     }
 })
